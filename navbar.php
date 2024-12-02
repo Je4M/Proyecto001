6921a0552c20689a96a5b363a009223fb37b1254
Michael-UNASAM <maicolkevinga@gmail.com>
2024-12-02 16:46:53 -0500

    Mensaje descriptivo de tus cambios

--- navbar.php
+++ navbar.php
@@ -47,7 +47,7 @@ error_log("Rol de usuario: " . print_r($rol, true));  // Inicializa $rol desde l
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link active" href="categorias.php">
+            <a class="nav-link active" href="coches.php">
                 <i class="fa fa-shopping-cart"></i>
                 coches
             </a>
